3832de94bbee8d4e8f0e655a7c59ce9c9080b606
Rexita-PC <bragazrexi@gmail.com>
2019-08-14 14:25:24 +0700

    first commit

---
+++ app/src/main/java/com/example/rexita_pc/coffe/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.rexita_pc.coffe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity =2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(quantity);
+        int price = quantity *5;
+        String priceMessage = "Total: $"+price+"\nThank you!";
+        displayMessage(priceMessage);
+
+    }
+
+    public void increment(View view) {
+        display(++quantity);
+    }
+
+    public void decrement(View view) {
+        display(--quantity);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+}
